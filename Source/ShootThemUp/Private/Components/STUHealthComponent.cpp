c7b64a6595932ae4161befe08ab7fa55702c0a0b
AshotMov <ashot.movsisyan.m@gmail.com>
2023-05-10 21:29:36 +0400

    Health Component

---
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -0,0 +1,33 @@
+// Shoot Them Up Game
+
+
+#include "Components/STUHealthComponent.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+USTUHealthComponent::USTUHealthComponent()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+// Called when the game starts
+void USTUHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Health = MaxHealth;
+
+    AActor* ComponentOwner = GetOwner();
+    if (ComponentOwner)
+    {
+        ComponentOwner->OnTakeAnyDamage.AddDynamic(this, &USTUHealthComponent::OnTakeAnyDamage);
+    }
+}
+
+
+void USTUHealthComponent::OnTakeAnyDamage(
+    AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
+{
+    Health -= Damage;
+}
